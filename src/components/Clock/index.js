c6de50eac12c41fd593cbc93699b357222e778a3
saierukala <sai.erukala1508@gmail.com>
2023-01-04 07:41:58 +0000

    show hideClock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,34 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  state = {date: new Date()}
+
+  componentDidMount() {
+    console.log('ComponentDidMount Called')
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount')
+    clearInterval(this.timerId)
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date(),
+    })
+  }
+
+  render() {
+    const {date} = this.state
+    console.log(date)
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
